522d8a970308b6cd103cf18f9abe3d68f2d95d24
Burak Saler <burakksaler@gmail.com>
2020-10-31 00:11:39 +0300

    Completed Item class

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,27 @@
 public class Item {
+	
+	private String id,name,category;
+	
+	public Item(String id, String name, String category) {
+		this.id = id;
+		this.name = name;
+		this.category = category;
+	}
+	
+	// Setters are not needed.
+	
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+	
+	
+	
 }
