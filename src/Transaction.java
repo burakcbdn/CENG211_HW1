e7de780d86078aca64c1856b3bd22b3a202a330c
Burak Saler <burakksaler@gmail.com>
2020-11-01 01:17:06 +0300

    complete transaction class

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,2 +1,26 @@
 public class Transaction {
+	
+	private final double purchasePrice;
+	private final double salePrice;
+	private final double numberOfSales;
+	
+	public Transaction(double purchasePrice, double salePrice, double numberOfSales) {
+		this.purchasePrice = purchasePrice;
+		this.salePrice = salePrice;
+		this.numberOfSales = numberOfSales;
+	}
+
+	public double getPurchasePrice() {
+		return purchasePrice;
+	}
+
+	public double getSalePrice() {
+		return salePrice;
+	}
+
+	public double getNumberOfSales() {
+		return numberOfSales;
+	}
+	
+	
 }
