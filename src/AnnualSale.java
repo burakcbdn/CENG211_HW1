f6744a1c15d73b5d0497f69fff3bf8e22f1603f4
Burak Saler <burakksaler@gmail.com>
2020-11-06 16:52:13 +0300

    complete AnnualSale Class

--- src/AnnualSale.java
+++ src/AnnualSale.java
@@ -1,2 +1,12 @@
 public class AnnualSale {
+	
+	ItemTransaction[] annualSale = new ItemTransaction[33];
+	
+	void addAnnualSale(ItemTransaction itemTransaction, int itemId) {
+		annualSale[itemId] = itemTransaction;
+	}
+	
+	ItemTransaction[] getAnnualSale() {
+		return annualSale;
+	}
 }
