41ee60cbeba872e59f4e196a0fc0c4e5ca52a152
Burak Saler <burakksaler@gmail.com>
2020-11-06 17:40:20 +0300

    decide variables and functions' publicity

--- src/AnnualSale.java
+++ src/AnnualSale.java
@@ -1,19 +1,19 @@
 public class AnnualSale {
 	
-	int sizeOfArray;
+	private int sizeOfArray;
 	
 	public AnnualSale(int numberOfItems) {
 		sizeOfArray = numberOfItems;
 	}
 
-	ItemTransaction[] annualSale = new ItemTransaction[sizeOfArray];
+	private ItemTransaction[] annualSale = new ItemTransaction[sizeOfArray];
 	
-	void addAnnualSale(ItemTransaction itemTransaction, int itemId) {
+	public void addAnnualSale(ItemTransaction itemTransaction, int itemId) {
 		annualSale[itemId] = itemTransaction;
 	}
 	
-	ItemTransaction[] getAnnualSale() {
-		return annualSale;
+	public ItemTransaction getAnnualSale(int itemId) {
+		return annualSale[itemId];
 	}
 	
 }
